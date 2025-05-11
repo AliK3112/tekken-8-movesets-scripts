50d6b8a01ec0e163331efe253d70c199df70aa56
Ali Khalil <alikhalil809@gmail.com>
2025-05-11 21:55:34 +0500

    scan addresses in the raw file

--- cpp/raw.cpp
+++ cpp/raw.cpp
@@ -12,22 +12,26 @@ enum RawFile
 
 GameClass game;
 
-uintptr_t PLAYER_STRUCT_BASE = 0x09AB9928;
-uintptr_t MOVESET_OFFSET = 0x3538;
+uintptr_t PLAYER_STRUCT_BASE = 0;
+uintptr_t MOVESET_OFFSET = 0;
+uintptr_t RAW_MOVESET_BASE = 0;
 
 uintptr_t getRawMovesetAddress(int side, DWORD offset);
 uintptr_t getMovesetAddress(uintptr_t playerAddr);
 uintptr_t getPlayerAddress(int side);
+void scanAddresses();
 
 int main()
 {
   if (game.Attach(L"Polaris-Win64-Shipping.exe"))
   {
+    scanAddresses();
+
     int player = 0;
     uintptr_t rawMoveset = getRawMovesetAddress(player, RawFile::Address);
-    uintptr_t fileSize = getRawMovesetAddress(player, RawFile::Size);
-    printf("0x%llx %d bytes\n", rawMoveset, fileSize);
-    return 0;
+    // uintptr_t fileSize = getRawMovesetAddress(player, RawFile::Size);
+    // printf("0x%llx %d bytes\n", rawMoveset, fileSize);
+    // return 0;
 
     uintptr_t moveset = getMovesetAddress(getPlayerAddress(player));
     uintptr_t movelist = game.readUInt64(moveset + Tekken::Offsets::MovesHeader);
@@ -40,6 +44,8 @@ int main()
       return (relativeAddr - moveset) + rawMoveset;
     };
 
+    // printf("INDEX NAME_KEY ANIM_KEY ANIM_IDX NAME_LENGTH ANIM_LENGTH\n");
+    printf("%5s %-10s %-10s %-10s %-11s %-11s %-10s %-10s\n", "INDEX", "NAME_KEY", "ANIM_KEY", "ANIM_IDX", "NAME_LENGTH", "ANIM_LENGTH", "ORDINAL_1", "ORDINAL_2");
     for (int i = 0; i < count; i++)
     {
       addr = movelist + i * Tekken::Sizes::Moveset::Move;
@@ -70,8 +76,8 @@ int main()
       printf("0x%.8x ", animKey);
       printf("0x%.8x ", animIdx);
       // printf("%s ", animRawIdx == i ? "Yes" : "No");
-      printf("%-5d ", nameLen - 1);
-      printf("%-5d ", animNameLen - 1);
+      printf("%-11d ", nameLen - 1);
+      printf("%-11d ", animNameLen - 1);
       printf("0x%.8x ", ordinalId1);
       printf("0x%.8x ", ordinalId2);
       printf("\n");
@@ -83,8 +89,7 @@ int main()
 
 uintptr_t getRawMovesetAddress(int side, DWORD offset)
 {
-  // TODO: Change first offset in the future
-  return game.getAddress({0x09AB2B08, 0x68, (DWORD)(0x2D0 + side * 0x10), 0x18, 0x8, 0x118, offset});
+  return game.getAddress({(DWORD)RAW_MOVESET_BASE, 0x68, (DWORD)(0x2D0 + side * 0x10), 0x18, 0x8, 0x118, offset});
 }
 
 uintptr_t getPlayerAddress(int side)
@@ -95,4 +100,54 @@ uintptr_t getPlayerAddress(int side)
 uintptr_t getMovesetAddress(uintptr_t playerAddr)
 {
   return game.ReadUnsignedLong(playerAddr + MOVESET_OFFSET);
+}
+
+void scanAddresses()
+{
+  printf("Scanning for addresses...\n");
+  uintptr_t addr = 0;
+  uintptr_t base = game.getBaseAddress();
+  uintptr_t start = base;
+
+  addr = game.FastAoBScan(Tekken::PLAYER_STRUCT_SIG_BYTES, start + 0x5A00000);
+  if (addr != 0)
+  {
+    start = addr; // To use as starting point for other scans
+
+    // $1 + $2 + $3 - $4
+    // $1 = Address at which the signature bytes were found
+    // $2 = Length of the instruction where signature bytes were found
+    // $3 = Relative offset to Player base address within the signature instruction
+    // $4 = Game's base address
+    PLAYER_STRUCT_BASE = addr + 7 + game.readUInt32(addr + 3) - base;
+  }
+  else
+  {
+    throw std::runtime_error("Match Struct Base Address not found!");
+  }
+
+  addr = game.FastAoBScan(Tekken::MOVSET_OFFSET_SIG_BYTES, base + 0x1800280);
+  if (addr != 0)
+  {
+    MOVESET_OFFSET = game.readUInt32(addr + 3);
+  }
+  else
+  {
+    throw std::runtime_error("\"Moveset\" Offset not found!");
+  }
+
+  addr = game.FastAoBScan(Tekken::RAW_MOVESET_FILE_PTR_SIG_BYTES, base + 0x5800000);
+  if (addr != 0)
+  {
+    RAW_MOVESET_BASE = addr + 11 + game.readUInt32(addr + 3) - base;
+  }
+  else
+  {
+    throw std::runtime_error("\"Raw Moveset Base\" Offset not found!");
+  }
+
+  printf("PLAYER_STRUCT_BASE: 0x%llX\n", PLAYER_STRUCT_BASE);
+  printf("MOVESET_OFFSET: 0x%llX\n", MOVESET_OFFSET);
+  printf("RAW_MOVESET_BASE: 0x%llX\n", RAW_MOVESET_BASE);
+  printf("Addresses successfully scanned...\n");
 }
