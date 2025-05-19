200e0341e205960ab3a2b856185cd1375d8e4d39
Ali Khalil <alikhalil809@gmail.com>
2025-05-20 00:33:34 +0500

    update throws script

--- throws.js
+++ throws.js
@@ -26,10 +26,11 @@ function listThrowsData() {
   // u1, throwextra_idx
   MOVESET.throws.forEach((throwData, i) => {
     const extradata = getThrowExtradata(throwData.throwextra_idx)
-    print(`--- Index ${i} - u1 ${throwData.u1} ---`)
+    let rows = []
     for (const data of extradata) {
-      print(data.u1, " - ", data.u2.map(x => hex(x, 4)).join(" "))
+      rows.push(`(${[data.u1, data.u2.map(x => hex(x, 4)).join(" ")].join(" ")})`)
     }
+    print(`${String(i).padStart(3, " ")}: (${throwData.u1}) - ${rows.join(" > ")}`)
   });
 }
 
@@ -40,7 +41,7 @@ function main() {
       print(moveset.tekken_character_name, "-", moveset.character_id);
     }
     MOVESET = moveset;
-    // if (moveset.character_id === 8) listThrowsData();
+    // if (moveset.character_id === 0) listThrowsData();
     listThrowsData();
   });
 }
