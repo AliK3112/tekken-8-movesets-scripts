388740bd1b6671d5530e32f11a406b5906ecfbe9
AliK3112 <alikhalil809@gmail.com>
2025-11-11 19:01:54 +0500

    updated toc script

--- toc.js
+++ toc.js
@@ -10,6 +10,27 @@ function padRight(str, len) {
   return str.toString().padEnd(len, " ");
 }
 
+function TkBinEntry(ctx, entryOffset) {
+  return {
+    size: 0x40,
+    value: {
+      signature: ctx.readUInt64(entryOffset),
+      decryptFlag: ctx.readUInt8(entryOffset + 0x8),
+      encKeyIndex: ctx.readUInt8(entryOffset + 0x9),
+      decryptionChecksum: ctx.readUInt8(entryOffset + 0xA),
+      decompressionChecksum: ctx.readUInt8(entryOffset + 0xB),
+      _0xC: ctx.readUInt32(entryOffset + 0xC),
+      hashSrc: ctx.readUInt32(entryOffset + 0x10),
+      _0x14: ctx.readUInt32(entryOffset + 0x14),
+      offset: ctx.readUInt32(entryOffset + 0x18),
+      size: ctx.readUInt32(entryOffset + 0x20),
+      size2: ctx.readUInt32(entryOffset + 0x28),
+      _0x30: ctx.readUInt32(entryOffset + 0x30),
+      _0x38: ctx.readUInt32(entryOffset + 0x38),
+    },
+  }
+}
+
 function main() {
   const stream = readFileSync(PATH);
   const reader = new BinaryFileReader(stream.buffer);
@@ -19,24 +40,47 @@ function main() {
   const listOffset = reader.readUInt32(0x10);
   print(`List Offset: ${hex(listOffset)}  List Size: ${listSize}`);
   print("");
+  print("Flag = Decryption Flag")
+  print("KIdx = Key Index. Used for picking a decryption key outta the key pool")
+  print("Chk1 = Checksum for Decryption")
+  print("Chk2 = Checksum for Decompression")
+  print("HashSrc = This is hashed (FNV-1a) to build the TOC")
+  print("Size2 = Allegedly, decrypted & uncompressed file size. This value is ALWAYS greater than `Size` unless Flg1 is 0")
+  print("");
+
+  // const dict = {};
+  // for (let i = 0; i < listSize; i++) {
+  //   const entryOffset = listOffset + i * 0x40;
+  //   const entry = reader.read(TkBinEntry, entryOffset);
+  //   // print(`#${i + 1} ${hex(entry.signature)} Off:${hex(entry.offset)} Size:${hex(entry.size)} DecFlag:${hex(entry.decryptFlag,2)} KeyIdx:${hex(entry.encKeyIndex,2)} DecChk:${hex(entry.decryptionChecksum,2)} DecmpChk:${hex(entry.decompressionChecksum,2)}`);
+
+  //   // Discovered that "hashSrc" isn't unique, despite being used as a hashing value
+  //   // TODO: Check if "hashSrc" is unique
+  //   const value = entry.hashSrc;
+  //   dict[value] ??= [];
+  //   dict[value].push(i+1);
+  // }
+
+  // Object.keys(dict).sort((a, b) => Number(a) - Number(b)).forEach(key => {
+  //   print(`HashSrc: ${hex(+key, 2)}  Entries: [${dict[key].join(", ")}]`);
+  // })
+
+  // return;
 
   // Define consistent column widths
   const colWidths = {
     Idx: 4,
     Signature: 14,
-    _0x8: 8,
-    _0x9: 8,
-    _0xA: 8,
-    _0xB: 8,
+    Flag: 8,
+    keyIdx: 8,
+    Chk1: 8,
+    Chk2: 8,
     _0xC: 14,
-    _0x10: 8,
-    _0x11: 8,
-    _0x12: 8,
-    _0x13: 8,
+    HashSrc: 14,
     _0x14: 14,
     Offset: 14,
     Size: 14,
-    _0x28: 14,
+    Size2: 14,
     _0x30: 22,
     _0x38: 22,
   };
@@ -44,19 +88,16 @@ function main() {
   const headerCols = [
     "Idx",
     "Signature",
-    "_0x8", // decryptionFlag
-    "_0x9", // aesKeyIndex
-    "_0xA", // decryptionChecksum
-    "_0xB", // decompressionChecksum
+    "Flag", // decryptionFlag
+    "keyIdx", // aesKeyIndex
+    "Chk1", // decryptionChecksum
+    "Chk2", // decompressionChecksum
     // "_0xC",
-    "_0x10", // FNV-1a hash index
-    "_0x11",
-    "_0x12",
-    "_0x13",
+    "HashSrc", // FNV-1a hash index
     // "_0x14",
     "Offset",
     "Size",
-    "_0x28", // uncompressed file size? this value is always less than the size when the decryption flag (0x8) is set to 0
+    "Size2", // uncompressed file size? this value is always less than the size when the decryption flag (0x8) is set to 0
     "_0x30",
     "_0x38"
   ];
@@ -77,10 +118,7 @@ function main() {
         _0xA: ctx.readUInt8(entryOffset + 0xA),
         _0xB: ctx.readUInt8(entryOffset + 0xB),
         _0xC: ctx.readUInt32(entryOffset + 0xC),
-        _0x10: ctx.readUInt8(entryOffset + 0x10),
-        _0x11: ctx.readUInt8(entryOffset + 0x11),
-        _0x12: ctx.readUInt8(entryOffset + 0x12),
-        _0x13: ctx.readUInt8(entryOffset + 0x13),
+        _0x10: ctx.readUInt32(entryOffset + 0x10),
         _0x14: ctx.readUInt32(entryOffset + 0x14),
         offset: ctx.readUInt32(entryOffset + 0x18),
         size: ctx.readUInt32(entryOffset + 0x20),
@@ -93,19 +131,16 @@ function main() {
     const row = [
       padRight((i + 1).toString(), colWidths.Idx),
       padRight(hex(entry.signature), colWidths.Signature),
-      padRight(hex(entry._0x8, 2), colWidths._0x8),
-      padRight(hex(entry._0x9, 2), colWidths._0x9),
-      padRight(hex(entry._0xA, 2), colWidths._0xA),
-      padRight(hex(entry._0xB, 2), colWidths._0xB),
+      padRight(hex(entry._0x8, 2), colWidths.Flag),
+      padRight(hex(entry._0x9, 2), colWidths.keyIdx),
+      padRight(hex(entry._0xA, 2), colWidths.Chk1),
+      padRight(hex(entry._0xB, 2), colWidths.Chk2),
       // padRight(hex(entry._0xC), colWidths._0xC),
-      padRight(hex(entry._0x10, 2), colWidths._0x10),
-      padRight(hex(entry._0x11, 2), colWidths._0x11),
-      padRight(hex(entry._0x12, 2), colWidths._0x12),
-      padRight(hex(entry._0x13, 2), colWidths._0x13),
+      padRight(hex(entry._0x10), colWidths.HashSrc),
       // padRight(hex(entry._0x14), colWidths._0x14),
       padRight(hex(entry.offset), colWidths.Offset),
       padRight(hex(entry.size), colWidths.Size),
-      padRight(hex(entry._0x28), colWidths._0x28),
+      padRight(hex(entry._0x28), colWidths.Size2),
       // padRight(entry.size < entry._0x28),
       padRight(hex(entry._0x30, 16), colWidths._0x30),
       padRight(hex(entry._0x38, 16), colWidths._0x38),
@@ -113,6 +148,7 @@ function main() {
 
     print(row.join(" "));
   }
+
 }
 
 main();
