48d2d5da894465c090d517c4b7698d2c4e2328cb
AliK3112 <alikhalil809@gmail.com>
2025-11-16 20:25:23 +0500

    updated utils

--- utils.js
+++ utils.js
@@ -116,6 +116,8 @@ const CHARACTER_NAMES = {
   35: '[HEIHACHI]',
   36: '[CLIVE]',
   37: '[ANNA]',
+  38: '[FAHKUMRAM]',
+  39: '[ARMOR_KING]',
   116: '[DUMMY]',
   117: '[ANGEL_JIN]',
   118: '[TRUE_DEVIL_KAZUYA]',
@@ -130,9 +132,82 @@ function camelToTitle(str) {
   return _.startCase(_.camelCase(str));
 }
 
+function fnv1a32FromBuffer(str) {
+  const FNV_OFFSET_BASIS = 0x811c9dc5; // 2166136261
+  const FNV_PRIME = 0x01000193;       // 16777619
+
+  let hash = FNV_OFFSET_BASIS >>> 0;
+  const buf = Buffer.from(str, 'utf8');
+
+  for (let i = 0; i < buf.length; i++) {
+    hash ^= buf[i];
+    // multiply by FNV_PRIME (32-bit overflow)
+    hash = (hash >>> 0) * FNV_PRIME >>> 0;
+    // (JavaScript does 64-bit float math, so we force 32-bit by >>>0)
+  }
+
+  return hash >>> 0; // ensure unsigned 32-bit
+};
+
+function getCharCode(charId) {
+  switch (charId) {
+    case 0:  return "grf"; // Paul
+    case 1:  return "pig"; // Law
+    case 2:  return "pgn"; // King
+    case 3:  return "cml"; // Yoshimitsu
+    case 4:  return "snk"; // Hwoarang
+    case 5:  return "rat"; // Xiayou
+    case 6:  return "ant"; // Jin
+    case 7:  return "cht"; // Bryan
+    case 8:  return "grl"; // Kazuya
+    case 9:  return "bsn"; // Steve
+    case 10: return "ccn"; // Jack8
+    case 11: return "der"; // Asuka
+    case 12: return "swl"; // DevilJin
+    case 13: return "klw"; // Feng
+    case 14: return "hms"; // Lili
+    case 15: return "kmd"; // Dragunov
+    case 16: return "ghp"; // Leo
+    case 17: return "lzd"; // Lars
+    case 18: return "mnt"; // Alisa
+    case 19: return "ctr"; // Claudio
+    case 20: return "hrs"; // Shaheen
+    case 21: return "kal"; // Nina
+    case 22: return "wlf"; // Lee
+    case 23: return "rbt"; // Kuma
+    case 24: return "ttr"; // Panda
+    case 25: return "crw"; // Zafina
+    case 26: return "jly"; // Leroy
+    case 27: return "aml"; // Jun
+    case 28: return "zbr"; // Reina
+    case 29: return "cat"; // Azucena
+    case 30: return "lon"; // Victor
+    case 31: return "bbn"; // Raven
+    case 32: return "got"; // Azazel
+    case 33: return "dog"; // Eddy
+    case 34: return "cbr"; // Lidia
+    case 35: return "bee"; // Heihachi
+    case 36: return "okm"; // Clive
+    case 37: return "kgr"; // Anna
+    case 38: return "tgr"; // Fahkumram
+    case 39: return "knk"; // Armor King
+    case 116: return "dek"; // Dummy
+    case 117: return "xxa"; // AngelJin
+    case 118: return "xxb"; // TrueDevilKazuya
+    case 119: return "xxc"; // Jack7
+    case 120: return "xxd"; // Soldier
+    case 121: return "xxe"; // DevilJin2
+    case 122: return "xxf"; // TekkenMonk
+    case 123: return "xxg"; // Seiryu
+    default:
+      return "Unknown";
+  }
+}
+
 module.exports = {
   print: (...args) => console.log(...args),
   hex,
+  getCharCode,
   toSignedInt32,
   getAllFiles,
   printInOrder,
@@ -140,6 +215,7 @@ module.exports = {
   getCharacterName,
   sortByGameId,
   camelToTitle,
+  fnv1a32FromBuffer,
   PATH,
   CHARACTER_NAMES,
 }
