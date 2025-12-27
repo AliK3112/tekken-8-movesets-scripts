82dede4672ad44976cbf192f2c062c7d1d1c891b
AliK3112 <alikhalil809@gmail.com>
2025-12-27 23:55:49 +0500

    reworked `hash.h` to not rely on "hexrays.h"

--- cpp/hash.h
+++ cpp/hash.h
@@ -1,16 +1,84 @@
-#include "hexrays.h"
+// #include "hexrays.h"
+// #include <cstdint>
+#include <string>
 
-// inline uint32_t __ROL4__(uint32_t value, uint8_t shift)
-// {
-//   return (value << shift) | (value >> (32 - shift));
-// }
+#ifndef HASH_TYPES_H
+#define HASH_TYPES_H
 
-// inline uint32_t __ROR4__(uint32_t value, uint8_t shift)
-// {
-//   return (value >> shift) | (value << (32 - shift));
-// }
+// Basic integer type aliases
+typedef          long long ll;
+typedef unsigned long long ull;
 
-// inline uint32_t _byteswap_ulong(uint32_t value)
+typedef unsigned int   uint;
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+typedef unsigned long  ulong;
+
+// Fixed-width integer type aliases
+typedef          char   int8;
+typedef   signed char   sint8;
+typedef unsigned char   uint8;
+typedef          short  int16;
+typedef   signed short  sint16;
+typedef unsigned short  uint16;
+typedef          int    int32;
+typedef   signed int    sint32;
+typedef unsigned int    uint32;
+typedef ll              int64;
+typedef ll              sint64;
+typedef ull             uint64;
+
+// Word definitions
+#define _WORD  uint16
+#define _DWORD uint32
+
+#endif // HASH_TYPES_H
+
+#define LAST_IND(x,part_type)    (sizeof(x)/sizeof(part_type) - 1)
+#define HIGH_IND(x,part_type)  LAST_IND(x,part_type)
+#define LOW_IND(x,part_type)   0
+
+#define WORDn(x, n)   (*((_WORD*)&(x)+n))
+#define DWORDn(x, n)  (*((_DWORD*)&(x)+n))
+
+#define LODWORD(x) DWORDn(x,LOW_IND(x,_DWORD))
+#define HIWORD(x)  WORDn(x,HIGH_IND(x,_WORD))
+
+// rotate left
+template<class T> T __ROL__(T value, int count)
+{
+  const uint nbits = sizeof(T) * 8;
+
+  if ( count > 0 )
+  {
+    count %= nbits;
+    T high = value >> (nbits - count);
+    if ( T(-1) < 0 ) // signed value
+      high &= ~((T(-1) << count));
+    value <<= count;
+    value |= high;
+  }
+  else
+  {
+    count = -count % nbits;
+    T low = value << (nbits - count);
+    value >>= count;
+    value |= low;
+  }
+  return value;
+}
+
+inline uint8  __ROL1__(uint8  value, int count) { return __ROL__((uint8)value, count); }
+inline uint16 __ROL2__(uint16 value, int count) { return __ROL__((uint16)value, count); }
+inline uint32 __ROL4__(uint32 value, int count) { return __ROL__((uint32)value, count); }
+inline uint64 __ROL8__(uint64 value, int count) { return __ROL__((uint64)value, count); }
+inline uint8  __ROR1__(uint8  value, int count) { return __ROL__((uint8)value, -count); }
+inline uint16 __ROR2__(uint16 value, int count) { return __ROL__((uint16)value, -count); }
+inline uint32 __ROR4__(uint32 value, int count) { return __ROL__((uint32)value, -count); }
+inline uint64 __ROR8__(uint64 value, int count) { return __ROL__((uint64)value, -count); }
+
+
+// inline uint32 _byteswap_ulong(uint32 value)
 // {
 //   return ((value >> 24) & 0x000000FF) | // Move byte 0 to byte 3
 //          ((value >> 8) & 0x0000FF00) |  // Move byte 1 to byte 2
@@ -18,43 +86,32 @@
 //          ((value << 24) & 0xFF000000);  // Move byte 3 to byte 0
 // }
 
-// inline uint32_t _LODWORD(uint64_t value)
-// {
-//   return static_cast<uint32_t>(value & 0xFFFFFFFF); // Mask the lower 32 bits
-// }
+int64 ComputeKamuiHash12To24(uint8 *a1, uint32 length);
 
-// inline uint32_t _HIWORD(uint64_t value)
-// {
-//   return static_cast<uint32_t>(
-//       value >> 32); // Shift right by 32 bits to get the upper 32 bits
-// }
-
-int64_t ComputeKamuiHash12To24(uint8_t *a1, uint32_t length);
-
-int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
+int64 ComputeKamuiHash(uint8 *a1, uint64 length)
 {
   int v2;       // ebx
-  uint8_t *v3;  // r11
+  uint8 *v3;  // r11
   int v4;       // eax
   int v5;       // r8d
   int v6;       // ecx
   int v7;       // eax
   int v8;       // eax
-  uint32_t v9;  // eax
-  uint8_t *v11; // r8
+  uint32 v9;  // eax
+  uint8 *v11; // r8
   int v12;      // eax
-  uint32_t v13; // eax
+  uint32 v13; // eax
   int v14;      // esi
   int v15;      // ebp
   int v16;      // edi
-  uint64_t v17; // rbx
-  uint8_t *v18; // r11
+  uint64 v17; // rbx
+  uint8 *v18; // r11
   int v19;      // r10d
   int v20;      // edx
   int v21;      // r8d
   int v22;      // r9d
   int v23;      // r10d
-  uint32_t v24; // edx
+  uint32 v24; // edx
   int v25;      // eax
 
   v2 = length;
@@ -68,7 +125,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
               __ROL4__(
                   0xCC9E2D51 *
                       (a1[length - 16] |
-                       ((a1[length - 15] | (*(uint16_t *)&a1[length - 14] << 8))
+                       ((a1[length - 15] | (*(uint16 *)&a1[length - 14] << 8))
                         << 8)),
                   15)) ^
                  (5 *
@@ -78,7 +135,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
                             __ROL4__(0xCC9E2D51 *
                                          (a1[length - 4] |
                                           ((a1[length - 3] |
-                                            (*(uint16_t *)&a1[length - 2] << 8))
+                                            (*(uint16 *)&a1[length - 2] << 8))
                                            << 8)),
                                      15)),
                        13) -
@@ -92,7 +149,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
               __ROL4__(
                   0xCC9E2D51 *
                       (a1[length - 12] |
-                       ((a1[length - 11] | (*(uint16_t *)&a1[length - 10] << 8))
+                       ((a1[length - 11] | (*(uint16 *)&a1[length - 10] << 8))
                         << 8)),
                   15)) ^
                  (5 *
@@ -102,7 +159,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
                             __ROL4__(0xCC9E2D51 *
                                          (a1[length - 8] |
                                           ((a1[length - 7] |
-                                            (*(uint16_t *)&a1[length - 6] << 8))
+                                            (*(uint16 *)&a1[length - 6] << 8))
                                            << 8)),
                                      15)),
                        13) -
@@ -116,7 +173,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
                           __ROL4__(0xCC9E2D51 *
                                        (a1[length - 20] |
                                         ((a1[length - 19] |
-                                          (*(uint16_t *)&a1[length - 18] << 8))
+                                          (*(uint16 *)&a1[length - 18] << 8))
                                          << 8)),
                                    15),
                   13) -
@@ -128,11 +185,11 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
       v19 = 0x1B873593 *
             __ROL4__(0xCC9E2D51 *
                          (*(v18 - 6) |
-                          ((*(v18 - 5) | (*((uint16_t *)v18 - 2) << 8)) << 8)),
+                          ((*(v18 - 5) | (*((uint16 *)v18 - 2) << 8)) << 8)),
                      15);
-      v20 = *(v18 - 2) | ((*(v18 - 1) | (*(uint16_t *)v18 << 8)) << 8);
+      v20 = *(v18 - 2) | ((*(v18 - 1) | (*(uint16 *)v18 << 8)) << 8);
       v21 = __ROL4__(v14 ^ v19, 14);
-      v22 = v18[10] | ((v18[11] | (*((uint16_t *)v18 + 6) << 8)) << 8);
+      v22 = v18[10] | ((v18[11] | (*((uint16 *)v18 + 6) << 8)) << 8);
       v23 = v19 - 0x3361D2AF * __ROL4__(v20 + v16, 13);
       v14 = v23;
       v24 = _byteswap_ulong(
@@ -144,7 +201,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
                     0x1B873593 *
                         __ROL4__(0xCC9E2D51 *
                                      (v18[6] |
-                                      ((v18[7] | (*((uint16_t *)v18 + 4) << 8))
+                                      ((v18[7] | (*((uint16 *)v18 + 4) << 8))
                                        << 8)),
                                  15)),
                13) -
@@ -154,7 +211,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
                     __ROL4__(
                         0xCC9E2D51 *
                             (v18[2] |
-                             ((v18[3] | (*((uint16_t *)v18 + 2) << 8)) << 8)),
+                             ((v18[3] | (*((uint16 *)v18 + 2) << 8)) << 8)),
                         15),
           14);
       v18 += 20;
@@ -162,8 +219,8 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
       v16 = 5 * _byteswap_ulong(v22 ^ (5 * (v25 - 0x52250EC)));
       // ORIGINAL DECOMPILED LINE
       LODWORD(v17) = v17 - 1;
-      // v17 = (static_cast<uint64_t>(_HIWORD(v17)) << 32) | (LODWORD(v17) - 1);
-    } while ((uint32_t)v17);
+      // v17 = (static_cast<uint64>(_HIWORD(v17)) << 32) | (LODWORD(v17) - 1);
+    } while ((uint32)v17);
     return 0xCC9E2D51 *
            __ROL4__(
                5 * __ROL4__(
@@ -189,7 +246,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
   }
   else if (length > 4)
   {
-    v11 = &a1[(uint32_t)length];
+    v11 = &a1[(uint32)length];
     v12 = __ROL4__(
         (5 *
          (__ROL4__(
@@ -209,7 +266,7 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
                    __ROL4__(0xCC9E2D51 *
                                 (5 * length +
                                  (*(v11 - 4) |
-                                  ((*(v11 - 3) | (*((uint16_t *)v11 - 1) << 8))
+                                  ((*(v11 - 3) | (*((uint16 *)v11 - 1) << 8))
                                    << 8))),
                             15)),
               13) -
@@ -218,15 +275,15 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
              __ROL4__(
                  0x318F97D9 -
                      0x3361D2AF *
-                         (a1[((uint32_t)length >> 1) & 4] |
-                          ((a1[(((uint32_t)length >> 1) & 4) + 1] |
-                            (*(uint16_t *)&a1[(((uint32_t)length >> 1) & 4) + 2]
+                         (a1[((uint32)length >> 1) & 4] |
+                          ((a1[(((uint32)length >> 1) & 4) + 1] |
+                            (*(uint16 *)&a1[(((uint32)length >> 1) & 4) + 2]
                              << 8))
                            << 8)),
                  15)),
         13);
     v13 = 0x85EBCA6B *
-          ((5 * (v12 - 0x52250EC)) ^ ((uint32_t)(5 * (v12 - 0x52250EC)) >> 16));
+          ((5 * (v12 - 0x52250EC)) ^ ((uint32)(5 * (v12 - 0x52250EC)) >> 16));
     return (0xC2B2AE35 * (v13 ^ (v13 >> 13))) ^
            ((0xC2B2AE35 * (v13 ^ (v13 >> 13))) >> 16);
   }
@@ -234,9 +291,9 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
   {
     v4 = 0;
     v5 = 9;
-    if ((uint32_t)length)
+    if ((uint32)length)
     {
-      length = (uint32_t)length;
+      length = (uint32)length;
       do
       {
         v6 = 0x3361D2AF * v4;
@@ -252,41 +309,41 @@ int64_t ComputeKamuiHash(uint8_t *a1, uint64_t length)
             (0x1B873593 * __ROL4__(0xCC9E2D51 * v4, 15)),
         13);
     v9 = 0x85EBCA6B *
-         ((5 * (v8 - 0x52250EC)) ^ ((uint32_t)(5 * (v8 - 0x52250EC)) >> 16));
+         ((5 * (v8 - 0x52250EC)) ^ ((uint32)(5 * (v8 - 0x52250EC)) >> 16));
     return (0xC2B2AE35 * (v9 ^ (v9 >> 13))) ^
            ((0xC2B2AE35 * (v9 ^ (v9 >> 13))) >> 16);
   }
 }
 
-int64_t ComputeKamuiHash12To24(uint8_t *a1,
-                               uint32_t length)
+int64 ComputeKamuiHash12To24(uint8 *a1,
+                               uint32 length)
 {
-  uint8_t *v2; // r11
-  uint8_t *v3; // r9
+  uint8 *v2; // r11
+  uint8 *v3; // r9
   int v4;      // edx
-  uint32_t v5; // edx
+  uint32 v5; // edx
 
   v2 = &a1[length];
-  v3 = &a1[(uint64_t)length >> 1];
+  v3 = &a1[(uint64)length >> 1];
   v4 = __ROL4__(
       (0x1B873593 *
        __ROL4__(
            0xCC9E2D51 *
-               (*(v2 - 4) | ((*(v2 - 3) | (*((uint16_t *)v2 - 1) << 8)) << 8)),
+               (*(v2 - 4) | ((*(v2 - 3) | (*((uint16 *)v2 - 1) << 8)) << 8)),
            15)) ^
           (5 *
            (__ROL4__(
                 (0x1B873593 *
                  __ROL4__(
                      0xCC9E2D51 *
-                         (*a1 | ((a1[1] | (*((uint16_t *)a1 + 1) << 8)) << 8)),
+                         (*a1 | ((a1[1] | (*((uint16 *)a1 + 1) << 8)) << 8)),
                      15)) ^
                     (5 * __ROL4__(
                              (0x1B873593 *
                               __ROL4__(
                                   0xCC9E2D51 *
                                       (*v3 |
-                                       ((v3[1] | (*((uint16_t *)v3 + 1) << 8))
+                                       ((v3[1] | (*((uint16 *)v3 + 1) << 8))
                                         << 8)),
                                   15)) ^
                                  (5 *
@@ -296,7 +353,7 @@ int64_t ComputeKamuiHash12To24(uint8_t *a1,
                                             0xCC9E2D51 *
                                                 (*(v2 - 8) |
                                                  ((*(v2 - 7) |
-                                                   (*((uint16_t *)v2 - 3) << 8))
+                                                   (*((uint16 *)v2 - 3) << 8))
                                                   << 8)),
                                             15)) ^
                                            (5 * __ROL4__(
@@ -305,7 +362,7 @@ int64_t ComputeKamuiHash12To24(uint8_t *a1,
                                                          0xCC9E2D51 *
                                                              (a1[4] |
                                                               ((a1[5] |
-                                                                (*((uint16_t *)
+                                                                (*((uint16 *)
                                                                        a1 +
                                                                    3)
                                                                  << 8))
@@ -323,7 +380,7 @@ int64_t ComputeKamuiHash12To24(uint8_t *a1,
                                                                             ((a1[(length >>
                                                                                   1) -
                                                                                  3] |
-                                                                              (*(uint16_t
+                                                                              (*(uint16
                                                                                      *)&a1
                                                                                    [(length >>
                                                                                      1) -
@@ -344,17 +401,17 @@ int64_t ComputeKamuiHash12To24(uint8_t *a1,
       13);
   v5 = 0xC2B2AE35 *
        ((0x85EBCA6B *
-         ((5 * (v4 - 0x52250EC)) ^ ((uint32_t)(5 * (v4 - 0x52250EC)) >> 16))) ^
+         ((5 * (v4 - 0x52250EC)) ^ ((uint32)(5 * (v4 - 0x52250EC)) >> 16))) ^
         ((0x85EBCA6B * ((5 * (v4 - 0x52250EC)) ^
-                        ((uint32_t)(5 * (v4 - 0x52250EC)) >> 16))) >>
+                        ((uint32)(5 * (v4 - 0x52250EC)) >> 16))) >>
          13));
   return v5 ^ HIWORD(v5);
 }
 
-int64_t ComputeKamuiHash(const char *a1, uint64_t length) {
-    return ComputeKamuiHash((uint8_t*)a1, length);
-}
+int64 ComputeKamuiStringHash(const char *a1, uint64 length) {
+  return ComputeKamuiHash((uint8*)a1, length);
+};
 
-int64_t ComputeKamuiHash(std::string a1, uint64_t length) {
-    return ComputeKamuiHash((uint8_t*)a1.c_str(), length);
-}
+int64 ComputeKamuiStringHash(std::string a1, uint64 length) {
+  return ComputeKamuiHash((uint8 *)a1.c_str(), length);
+};
