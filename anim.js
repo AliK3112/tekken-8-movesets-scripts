9171bb48cc217629b8e5e227b0fbba7c5e577290
AliK3112 <alikhalil809@gmail.com>
2026-02-14 17:16:47 +0500

    revamped anim.js

--- anim.js
+++ anim.js
@@ -3,33 +3,90 @@
 const fs = require("fs");
 const BinaryFileReader = require("./binaryFileReader");
 
-const hex = (x, length = 8) => "0x" + x.toString(16).padStart(length, "0");
+// const hex = (x, length = 8) => "0x" + x.toString(16).padStart(length, "0");
+const hex = (x, length = 8) => x.toString(16).padStart(length, "0");
+const hexL = (x) => hex(x, 16);
 const print = console.log;
 
-const path = "Binary\\mothead\\bin\\com.anmbin";
-// const data = require(path);
-// console.log(data);
+const TITLES = ["BODY", "HAND", "FACE", "WINGS", "CAMERA", "EXTRA-BODY"];
+
+const PATH = "Binary\\mothead\\bin\\com.anmbin";
 
 function main() {
-  const file = fs.readFileSync(path);
+  const file = fs.readFileSync(PATH);
   const reader = new BinaryFileReader(file.buffer);
 
   const readLong = (offset) => Number(reader.readUInt64(offset));
 
-  // reader.skip(4);
-  // const counts = Array(12).fill(0).map(() => reader.readUInt32());
-  // print(counts);
-  // print(hex(reader.getPosition()));
-  // reader.skip(4);
-  const count = reader.readUInt32(0x4);
-  const offset = readLong(0x38);
+  const readIntArray = (len) =>
+    Array(len)
+      .fill(0)
+      .map(() => reader.readUInt32());
 
-  // print("Total:" count, hex(offset));
+  const readLongArray = (len) =>
+    Array(len)
+      .fill(0)
+      .map(() => readLong());
 
-  reader.seek(offset);
-  for (let i = 0; i < count; i++) {
-    const animKey = reader.readUInt32(offset + i * 56);
-    print(i, hex(animKey), animKey);
+  reader.skip(4);
+  const blocksCount = readIntArray(6);
+  const listsCount = readIntArray(6);
+  reader.skip(4);
+  const blockOffsets = readLongArray(6);
+  const listOffsets = readLongArray(6);
+
+  const printEverything = false;
+
+  // PRINTING BLOCKS INFORMATION
+  print("===== BLOCKS =====");
+  for (let i = 0; i < 6; i++) {
+    const offset = blockOffsets[i];
+    const count = blocksCount[i];
+    reader.seek(offset);
+    print("BLOCK:", TITLES[i], hex(offset), count);
+    for (let j = 0; j < count; j++) {
+      if (printEverything) {
+        const values = readIntArray(14);
+        const printVals = values.map(x => hex(x)).join(" ");
+
+        // const values = readLongArray(7);
+        // const printVals = values.map(x => hexL(x)).join(" ");
+
+        print(
+          j.toString().padStart(5, " "),
+          printVals,
+        );
+      } else {
+        const animKey = reader.readUInt32(); // 0x0
+        reader.skip(4);
+        const animOffset = readLong(); // 0x8
+        reader.skip(8); // 0x10
+        const flags = readIntArray(6);
+        reader.skip(8); // 0x30
+
+        const values = [
+          hex(animKey),
+          hexL(animOffset),
+          flags.map((x) => hex(x)).join(" "),
+        ];
+        print(j.toString().padStart(5, " "), values.join(" "));
+      }
+    }
+    print();
+  }
+
+  // PRINTING LISTS INFORMATION
+  print("===== LISTS =====");
+  for (let i = 0; i < 6; i++) {
+    const offset = listOffsets[i];
+    const count = listsCount[i];
+    reader.seek(offset);
+    print("LIST:", TITLES[i], hex(offset), count);
+    for (let j = 0; j < count; j++) {
+      const values = [reader.readUInt32()];
+      print(j.toString().padStart(5, " "), values.map((x) => hex(x)).join(" "));
+    }
+    print();
   }
 }
 
