38ef4a0bb9bbc91b103a38ccd6c7f22ab2970ca2
AliK3112 <alikhalil809@gmail.com>
2026-02-14 17:25:56 +0500

    rework "main" logic and support CL arguments

--- anim.js
+++ anim.js
@@ -10,10 +10,10 @@ const print = console.log;
 
 const TITLES = ["BODY", "HAND", "FACE", "WINGS", "CAMERA", "EXTRA-BODY"];
 
-const PATH = "Binary\\mothead\\bin\\com.anmbin";
+const FOLDER_PATH = "Binary\\mothead\\bin\\";
 
-function main() {
-  const file = fs.readFileSync(PATH);
+function processAnimBin(filePath) {
+  const file = fs.readFileSync(filePath);
   const reader = new BinaryFileReader(file.buffer);
 
   const readLong = (offset) => Number(reader.readUInt64(offset));
@@ -47,22 +47,19 @@ function main() {
     for (let j = 0; j < count; j++) {
       if (printEverything) {
         const values = readIntArray(14);
-        const printVals = values.map(x => hex(x)).join(" ");
+        const printVals = values.map((x) => hex(x)).join(" ");
 
         // const values = readLongArray(7);
         // const printVals = values.map(x => hexL(x)).join(" ");
 
-        print(
-          j.toString().padStart(5, " "),
-          printVals,
-        );
+        print(j.toString().padStart(5, " "), printVals);
       } else {
         const animKey = reader.readUInt32(); // 0x0
         reader.skip(4);
         const animOffset = readLong(); // 0x8
         reader.skip(8); // 0x10
-        const flags = readIntArray(6);
-        reader.skip(8); // 0x30
+        const flags = readIntArray(4);
+        reader.skip(16);
 
         const values = [
           hex(animKey),
@@ -87,6 +84,22 @@ function main() {
       print(j.toString().padStart(5, " "), values.map((x) => hex(x)).join(" "));
     }
     print();
+  }
+}
+
+function main() {
+  const files = fs
+    .readdirSync(FOLDER_PATH)
+    .filter((file) => file.endsWith(".anmbin"));
+
+  const charCode = process.argv[2];
+
+  for (const file of files) {
+    if (charCode && !file.includes(charCode)) continue;
+
+    print("Processing File: %s", file);
+
+    processAnimBin(`${FOLDER_PATH}/${file}`)
   }
 }
 
