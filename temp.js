3f8ca4743cd4dc164cc7727312af70ed2bbe3de0
AliK3112 <alikhalil809@gmail.com>
2026-06-03 03:42:11 +0500

    temp script updated with more prefixed paths

--- temp.js
+++ temp.js
@@ -5,8 +5,8 @@ const { readMovesList } = require("./utils");
 
 const print = console.log;
 
-// const hex = (value, length = 2) =>
-//   "0x" + value.toString(16).padStart(length, "0");
+const hex = (value, length = 2) =>
+  "0x" + value.toString(16).padStart(length, "0");
 
 // // const readTxt = (path) => {
 // //   const buffer = fs.readFileSync(path, "utf-8");
@@ -46,122 +46,112 @@ const print = console.log;
 // //   }
 // // };
 
-// // const FILE = "crw";
+// const FILE = "ker";
 
-// // (() => {
-// //   // restoreNames();
-// //   // return;
-// //   const buffer = fs.readFileSync(`./Binary/mothead/bin/${FILE}.motbin`);
-// //   const reader = new BinaryFileReader(buffer.buffer);
+// (() => {
+//   // restoreNames();
+//   // return;
+//   const buffer = fs.readFileSync(`./Binary/mothead/bin/${FILE}.motbin`);
+//   const reader = new BinaryFileReader(buffer.buffer);
 
-// //   const dict = require("./name_keys.json");
+//   const dict = require("./name_keys.json");
 
-// //   const moves = readMovesList(reader);
+//   const moves = readMovesList(reader);
 
-// //   // This value will always be less than "count" as it's an index
-// //   const alias8000 = reader.readInt16(0x30);
-// //   const alias8001 = reader.readInt16(0x32);
+//   // This value will always be less than "count" as it's an index
+//   const alias8000 = reader.readInt16(0x30);
+//   const alias8001 = reader.readInt16(0x32);
 
-// //   // TODO: Take all the moves before alias8000
-// //   // Append "_story" at their end and generate their hash (e.g, hash.computeKamuiHash(name))
-// //   // See if we find a match for that hash after b/w alias8000 and the end of the movelist
-// //   const aliasMatches = [];
-// //   let flag = false;
+//   // TODO: Take all the moves before alias8000
+//   // Append "_story" at their end and generate their hash (e.g, hash.computeKamuiHash(name))
+//   // See if we find a match for that hash after b/w alias8000 and the end of the movelist
+//   const aliasMatches = [];
+//   let flag = false;
 
-// //   for (let i = 0; i < moves.length; i++) {
-// //     const move = moves[i];
-// //     if (!move.name) continue;
+//   for (let i = 0; i < moves.length; i++) {
+//     const move = moves[i];
+//     if (!move.name) continue;
 
-// //     let storyName;
-// //     // storyName = String(move.name).replace("Kz_", "Xxb_");
-// //     // storyName = "Esc_" + move.name;
-// //     storyName = "" + move.name + "3";
-// //     // storyName = "" + move.name + "_BIG";
-// //     // storyName = "" + move.name + "_NewDASH";
-// //     // storyName = "story_" + move.name;
-// //     // storyName = String(move.name).replace("_tw", "_2_tw");
-// //     // storyName = String(move.name).replace("3", "4");
-// //     const storyHash = Number(computeKamuiHash(storyName));
+//     const prefixes = ["_hit", "_wall", "_armor", "2nd", "3rd", "_n", "_y", "_Turn", "_put", "_guard", "_Back", "_end", "_shoryu"];
 
-// //     // if (move.name === "Kz_4rprp") {
-// //     //   print("~FOUND!");
-// //     //   print("Key:", hex(move.name_key));
-// //     //   print("Story Name:", storyName);
-// //     //   print("Story Key:", storyHash);
-// //     // }
+//     for (const prefix of prefixes) {
+//       let storyName;
+//       storyName = move.name + prefix;
+//       const storyHash = Number(computeKamuiHash(storyName));
 
-// //     // Look for matching name_key in the alias region
-// //     for (let j = 0; j < moves.length; j++) {
-// //       if (moves[j].name_key === storyHash && !dict[storyHash]) {
-// //         // if (moves[j].name_key === storyHash) {
-// //         flag = true;
-// //         dict[storyHash] = storyName;
-// //         aliasMatches.push({
-// //           base_index: i,
-// //           base_name: move.name,
-// //           story_name: storyName,
-// //           story_hash: hex(storyHash),
-// //           alias_index: j,
-// //           message: `"${storyHash}": "${storyName}",`,
-// //         });
-// //       }
-// //     }
-// //   }
+//       // Look for matching name_key in the alias region
+//       for (let j = 0; j < moves.length; j++) {
+//         if (moves[j].name_key === storyHash && !dict[storyHash]) {
+//           // if (moves[j].name_key === storyHash) {
+//           flag = true;
+//           dict[storyHash] = storyName;
+//           aliasMatches.push({
+//             base_index: i,
+//             base_name: move.name,
+//             story_name: storyName,
+//             story_hash: hex(storyHash),
+//             alias_index: j,
+//             message: `"${storyHash}": "${storyName}",`,
+//           });
+//         }
+//       }
+//     }
+//   }
 
-// //   // Print or use result
-// //   print("Found alias matches:", aliasMatches);
+//   // Print or use result
+//   print("Found alias matches:", aliasMatches);
 
-// //   aliasMatches.forEach((alias) => {
-// //     print(alias.message);
-// //   });
+//   aliasMatches.forEach((alias) => {
+//     print(alias.message);
+//   });
 
-// //   // If new entry found, write to the dictionary
-// //   if (flag) {
-// //     fs.writeFileSync("./name_keys.json", JSON.stringify(dict, null, 2));
-// //   }
-// // })();
+//   // If new entry found, write to the dictionary
+//   if (flag) {
+//     fs.writeFileSync("./name_keys.json", JSON.stringify(dict, null, 2));
+//   }
+// })();
 
-// // // function readCharMoves(charCode) {
-// // //   const file = fs.readFileSync(`./Binary/mothead/bin/${charCode}.motbin`);
-// // //   const reader = new BinaryFileReader(file.buffer);
-// // //   const moves = readMovesList(reader);
-// // //   return moves;
-// // // }
+// function readCharMoves(charCode) {
+//   const file = fs.readFileSync(`./Binary/mothead/bin/${charCode}.motbin`);
+//   const reader = new BinaryFileReader(file.buffer);
+//   const moves = readMovesList(reader);
+//   return moves;
+// }
 
-// // // (() => {
-// // //   const mvl1 = readCharMoves("bsn");
-// // //   const mvl2 = readCharMoves("xxd");
-// // //   const dict = require("./name_keys.json");
-// // //   let flag = false;
+// (() => {
+//   const mvl1 = readCharMoves("bsn");
+//   const mvl2 = readCharMoves("xxd");
+//   const dict = require("./name_keys.json");
+//   let flag = false;
 
-// // //   for (let i = 0; i < mvl1.length; i++) {
-// // //     const move = mvl1[i];
-// // //     if (!move.name) continue;
+//   for (let i = 0; i < mvl1.length; i++) {
+//     const move = mvl1[i];
+//     if (!move.name) continue;
 
-// // //     let storyName = "";
-// // //     storyName = move.name + "_tw";
-// // //     const storyHash = computeKamuiHash(storyName);
+//     let storyName = "";
+//     storyName = move.name + "_tw";
+//     const storyHash = computeKamuiHash(storyName);
 
-// // //     for (let j = 0; j < mvl2.length; j++) {
-// // //       if (mvl2[j].name_key === storyHash && !dict[storyHash]) {
-// // //         flag = true;
-// // //         dict[storyHash] = storyName;
-// // //         print(`"${storyHash}": "${storyName}",`);
-// // //       }
-// // //     }
-// // //   }
+//     for (let j = 0; j < mvl2.length; j++) {
+//       if (mvl2[j].name_key === storyHash && !dict[storyHash]) {
+//         flag = true;
+//         dict[storyHash] = storyName;
+//         print(`"${storyHash}": "${storyName}",`);
+//       }
+//     }
+//   }
 
-// // //   // If new entry found, write to the dictionary
-// // //   if (flag) {
-// // //     fs.writeFileSync("./name_keys.json", JSON.stringify(dict, null, 2));
-// // //   }
-// // // })();
+//   // If new entry found, write to the dictionary
+//   if (flag) {
+//     fs.writeFileSync("./name_keys.json", JSON.stringify(dict, null, 2));
+//   }
+// })();
 
 // Script to restore some anim name keys
 
 const SET_SMALL = "abcdefghijklmnopqrstuvwxyz";
 
-// Helper to generate all combinations of a given length from chars
+/* Helper to generate all combinations of a given length from chars */
 function* generateSuffixes(chars, length, prefix = "") {
   if (length === 0) {
     yield prefix;
@@ -185,6 +175,7 @@ const tk_charId = (c) => ({
     const filePath = `./Binary/mothead/bin/${file}`;
     const code = file.replace(".motbin", "");
     if (code === "test") continue;
+    // if (code !== "grl") continue;
     const buffer = fs.readFileSync(filePath);
     const reader = new BinaryFileReader(buffer.buffer);
     const charId = reader.read(tk_charId);
@@ -219,6 +210,16 @@ const tk_charId = (c) => ({
 
     for (const gen of generateSuffixes(SET_SMALL, 2)) {
       addToDict(code + gen + "_co_kamae");
+      addToDict(code + gen + "_co_step_b");
+      addToDict(code + gen + "_co_step_l");
+      addToDict(code + gen + "_co_step_r");
+      addToDict(code + gen + "_co_crouch");
+      addToDict(code + gen + "_co_crouchf");
+      addToDict(code + gen + "_co_walkf");
+      addToDict(code + gen + "_co_syagami");
+      addToDict(code + gen + "_co_stand");
+      addToDict(code + gen + "_co_standb");
+      addToDict(code + gen + "_co_standf");
       addToDict(code + gen + "_ra_pre");
       addToDict(code + gen + "_ra_finish_f");
       addToDict(code + gen + "_ra_finish_y");
@@ -227,6 +228,7 @@ const tk_charId = (c) => ({
       addToDict(code + gen + "_ra_pre_story");
       addToDict(code + gen + "_ra_finish_story_f");
       addToDict(code + gen + "_ra_finish_story_y");
+      addToDict(code + gen + "_at_drive");
       addToDict(code + gen + "_at_zd");
       addToDict(code + gen + "_at_zd_miss");
       addToDict(code + gen + "_th_zd_f");
@@ -239,6 +241,8 @@ const tk_charId = (c) => ({
       addToDict(code + gen + "_th_hs_y");
       addToDict(code + gen + "_th_hs_hit_f");
       addToDict(code + gen + "_th_hs_hit_y");
+      addToDict(code + gen + "_at_hs_hit_f");
+      addToDict(code + gen + "_at_hs_hit_y");
 
       addToDict(code + gen + "_at_lp");
       addToDict(code + gen + "_at_rp");
@@ -260,13 +264,19 @@ const tk_charId = (c) => ({
       addToDict(code + gen + "_at_strk");
       addToDict(code + gen + "_at_stwp");
       addToDict(code + gen + "_at_stwk");
+      addToDict(code + gen + "_at_sylp");
+      addToDict(code + gen + "_at_syrp");
+      addToDict(code + gen + "_at_sylk");
+      addToDict(code + gen + "_at_syrk");
+      addToDict(code + gen + "_at_sywp");
+      addToDict(code + gen + "_at_sywk");
 
       const inputs = ["lp", "rp", "lk", "rk", "wp", "wk"];
       const depth = 2;
       // Recursively generate all possible input combos of specified depth, using self, and store hashes.
       function generateAndStoreCombinations(prefix, combo = "", d = 0) {
         if (d === depth) {
-          const animName = code + gen + "_at_st" + combo;
+          const animName = code + gen + "_at_" + combo;
           addToDict(animName);
           return;
         }
